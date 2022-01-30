8cfc53b1119c5bfff7dbdab40909031fb2b68d50
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-01-29 19:57:34 -0600

    Rewind: App Page and Misc Changes
    Changes:
    
    1. Built out UI for App page
    2. Built out logic for App page using JS
    3. Adjusted the navbar to include the project name, Rewind, in Racing Sans One font and did the same on the landing page and code pages, too.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-// call Hello World
-function HelloWorld() {
-  alert("Hello World!");
+
+// get the string from the UI
+
+function getString() {
+  // get string from the page
+  let userString = document.getElementById('userInput').value;
+  // call reverseString
+  let reversedString = reverseString(userString);
+  // console.log(reversedString);
+  displayString(reversedString);
+}
+
+// generate the string in reverse
+function reverseString(str) {
+  let len = str.length;
+  let result = "";
+
+  for(let i = len - 1; i >= 0; i--){
+    result += str[i];
+  }
+
+  return result;
+}
+
+// display the result
+function displayString(str) {
+
+  document.getElementById('results').innerHTML = `<tr><td><b>${str}</b></td></tr>`;
+
 }
