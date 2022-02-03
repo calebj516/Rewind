40cc9a6c5e5c7ae1ac92721826a61b0104ea9ed5
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-02-02 21:32:41 -0600

    Rewind: The Code
    Built out The Code page, using prism js to show the code. I included descriptions of each function, as well.

--- js/site.js
+++ js/site.js
@@ -1,8 +1,7 @@
-
 // get the string from the UI
 // controller function
 function getString() {
-
+  // hide the element displaying the result to start off.
   document.getElementById('alert').classList.add('invisible');
   // get string from the page
   let userString = document.getElementById('userInput').value;
@@ -14,23 +13,21 @@ function getString() {
 // generate the string in reverse
 // logic function
 function reverseString(str) {
-
+  // array to hold result
   let reversed = [];
-
+  // loop through and push each element of str to reversed, starting from the last index to the first.
   for(let i = str.length - 1; i >= 0; i--){
     reversed.push(str[i]);
   }
-
-  return reversed;
+  // return the reversed array, joined back into a string.
+  return reversed.join('');
 }
 
 // display the result
 // view function
 function displayString(revString) {
-
   // write to the page
-  document.getElementById('msg').innerHTML = `Your string reversed is: ${revString.join('')}`;
+  document.getElementById('msg').innerHTML = `Your string reversed is: ${revString}`;
   // show the alert box
   document.getElementById('alert').classList.remove('invisible');
-
 }
